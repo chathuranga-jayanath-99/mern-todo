795462f981a0147dd35d63ce5bb8fea0cdd75c24
Chathuranga Jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-07-02 18:37:56 +0530

    separate routes from server.js

---
+++ server/routes/todos.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        msg: "GET all todos",
+    });
+});
+
+router.get("/:id", (req, res) => {
+    res.json({
+        msg: "GET a single todo",
+    });
+});
+
+router.post("/", (req, res) => {
+    res.json({
+        msg: "POST a new todo",
+    });
+});
+
+router.delete("/:id", (req, res) => {
+    res.json({
+        msg: "DELETE a todo",
+    });
+});
+
+router.patch("/:id", (req, res) => {
+    res.json({
+        msg: "UPDATE a todo",
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const todoRouter = require("./routes/todos");
 
 const app = express();
 
+app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 
-app.get("/", (req, res) => {
-    res.json({
-        msg: "Welcome to Todo App",
-    });
-});
+app.use("/api/todos", todoRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}!!!`);
